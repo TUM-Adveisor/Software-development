65e2fb99fd04acd8a45bd8b389eaf5cd43aa2f2a
Matthias Bitterle <m.k.bitterle@0gmail.com>
2022-06-10 03:11:46 +0200

    w

--- datengenerieren.py
+++ datengenerieren.py
@@ -1,3 +1,7 @@
 import cv2
-
-
+ 
+# read image as grey scale
+grey_img = cv2.imread('/home/pi/Adveisor/Software/Software/adveisor.png', cv2.IMREAD_GRAYSCALE)
+ 
+# save image
+status = cv2.imwrite('//home/pi/Adveisor/Software/Software/adveisor.png',grey_img)
