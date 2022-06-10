7c4440faac0577cbe68fa650573bd4e5870dac58
Matthias Bitterle <m.k.bitterle@0gmail.com>
2022-06-10 03:25:34 +0200

    w

--- datengenerieren.py
+++ datengenerieren.py
@@ -1,4 +1,11 @@
 import cv2
+import games
+import config
+import player #can be delatet later
+
+#mport bewegungsystem
+#config=config.Config()
+#b=bewegungsystem.Bewegungssystem(config)
 camera = cv2.VideoCapture(0)
 return_value, image = camera.read()
 cv2.imwrite('opencv'+str(1)+'.png', image)
