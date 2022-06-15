6c34b59a9843f03dfc98137d3d31ff39f8831220
Matthias Bitterle <m.k.bitterle@gmail.com>
2022-06-15 17:12:00 +0200

    datengenerieren finished

--- bewegungsystem.py
+++ bewegungsystem.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(1, '/home/pi/Adveisor/Hardware/gcodelib')
+sys.path.insert(1, '/home/pi/Desktop/Adveisor/Hardware/gcodelib')
 import gcodelib
 import time
 
@@ -12,9 +12,13 @@ class Bewegungssystem():
         self.config=config
         self.connection = gcodelib.Connection("/dev/ttyUSB0")
         self.connection.console_read()
+        self.aktuel=[0,0]
+        self.neu=[0,0]
         
         self.connection.home()
     def bewege_von_nach(self,x_start,y_start,x_end,y_end, note):
+        self.neu[0]=x_start
+        self.neu[1]=y_start
         #self.nl=self.nl+1
         #print(self.nl)
         if y_start<0 or x_start<0:
@@ -23,13 +27,16 @@ class Bewegungssystem():
             print(note)
             self.connection.magnet_control(False)
             #print("magnet off")
-            self.connection.send_coords(x_start,y_start)
-            self.connection.wait_for_movement_start()
-            #print("header started")
-            while (self.connection._status != "Idle"):
-                #self.connection.print_coords()
-                time.sleep(1/10)
-            #print("header stopped")
+            if self.neu!=self.aktuel:
+            
+                
+                self.connection.send_coords(x_start,y_start)
+                self.connection.wait_for_movement_start()
+                #print("header started")
+                while (self.connection._status != "Idle"):
+                    #self.connection.print_coords()
+                    time.sleep(1/10)
+                #print("header stopped")
 
             self.connection.magnet_control(True)
             #print("magnet on")
@@ -42,4 +49,6 @@ class Bewegungssystem():
             #print("header stopped")
             self.connection.magnet_control(False)
             #print("magnet off")
+            self.aktuel[0]=x_end
+            self.aktuel[1]=y_end
     
--- datengenerieren.py
+++ datengenerieren.py
@@ -1,11 +1,59 @@
 import cv2
 import games
 import config
-import player #can be delatet later
-
+import player 
 import bewegungsystem
+import random
+import time
+bewegungsytem=None
+feldgröße_y=1
+feldgröße_x=1
+felderstart_x=1
+felderstart_y=1
+start_pos_y=1
+start_pox_x=1
+max_varation=10
+
 config=config.Config()
 b=bewegungsystem.Bewegungssystem(config)
+b.bewege_von_nach(40,40,300,300,"test")
+b.bewege_von_nach(300,300,400,400,"test")
+
+#b.bewege_von_nach(300,300,30,30,"test")
+"""""time.sleep(1)
 camera = cv2.VideoCapture(0)
-return_value, image = camera.read()
-cv2.imwrite('opencv'+str(1)+'.png', image)
+XY_Matrix=[]
+def real_to_logic_postion(x_pos,y_pos):
+        höhe_feld=feldgröße_x
+        breite_feld=feldgröße_y
+
+        for i in range(8):
+        
+            if y_pos // höhe_feld<i:
+                y_pos=i
+        
+        for i in range(8):
+            if x_pos // breite_feld <i:
+                x_pos=i
+        return[x_pos,y_pos]
+
+def logic_to_real_postion(x_pos,y_pos):
+
+    real_postion_y=felderstart_x+((y_pos)*feldgröße_y)
+    real_postion_x=felderstart_x+((x_pos)*feldgröße_x)
+    return [real_postion_x,real_postion_y]
+
+for x in range(8):
+    for y in range(8):
+        for i in range(1):#10 einfügen
+            rgb=[random.randint(0,255),random.randint(0,255),random.randint(0,255)]
+            end=logic_to_real_postion((x+(random.randint(0,max_varation))),(y+(random.randint(0,max_varation))))
+            print(x,y)
+            b.bewege_von_nach(start_pox_x,start_pos_y,end[0],end[1],"test")
+            start_pox_x=end[0]
+            start_pos_y=end[1]
+            #return_value, image = camera.read()
+            #cv2.imwrite(str(x)+str(y)+str(i)+'figurenname+'+''.png', image)                                         
+                
+                
+                """
